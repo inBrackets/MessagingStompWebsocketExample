f73e3d7872f3ede8f6f32c9c8ce6304c963b8233
Jason <extremewinds@gmail.com>
2025-08-03 22:16:43 +0200

    Initial commit

---
+++ src/test/java/org/example/messagingstompwebsocketexample/MessagingStompWebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.messagingstompwebsocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompWebsocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
